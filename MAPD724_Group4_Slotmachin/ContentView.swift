d8ddaebda990d2313aeab77666e6965f284323f1
Ridham3097 <ridhamppatel26@outlook.com>
2022-01-25 01:52:35 +0530

    Initial Commit

---
+++ MAPD724_Group4_Slotmachin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MAPD724_Group4_Slotmachin
+//
+//  Created by Ridham Patel on 2022-01-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MAPD724_Group4_Slotmachin/MAPD724_Group4_SlotmachinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAPD724_Group4_SlotmachinApp.swift
+//  MAPD724_Group4_Slotmachin
+//
+//  Created by Ridham Patel on 2022-01-25.
+//
+
+import SwiftUI
+
+@main
+struct MAPD724_Group4_SlotmachinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
